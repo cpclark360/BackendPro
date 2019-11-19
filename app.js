a9fe9f5e7a4472cf331759f6833298afb4ac0ee6
Cedric Clark <40728556+cpclark360@users.noreply.github.com>
2019-11-18 20:51:33 -0600

    added Backend files

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// routes
+const contacts = require("./routes/api/contacts");
+
+const app = express();
+// CORS set header to allow all complex requests
+app.options("*", cors());
+
+// cors origin URL - Allow inbound traffic from origin
+corsOptions = {
+  origin: "https://portfoliofrontend.herokuapp.com",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+// Connect Database
+connectDB();
+
+//  Middleware
+app.use(express.json({ extended: false }));
+// app.use(morgan("dev"));
+
+// app.get("/", (req, res) => {
+//   res.json({ success: true });
+// });
+
+app.use("/api/contacts", contacts);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
